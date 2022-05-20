bc47d81b7b07f94229d8cb095a28d264ccb7593d
markiegaming <58847729+markiegaming@users.noreply.github.com>
2022-05-20 13:14:02 +0200

    update 2: new table
    er is een nieuwe tabel aangemaakt en een andere gedeleten

--- ContosoUniversity/Data/DbInitializer.cs
+++ ContosoUniversity/Data/DbInitializer.cs
@@ -18,14 +18,14 @@ namespace ContosoUniversity.Data
 
             var users = new users[]
             {
-            new users{FirstMidName="among us",LastName="sussy impasta",EnrollmentDate=DateTime.Parse("2005-09-01")},
-            new users{FirstMidName="Meredith",LastName="Alonso",EnrollmentDate=DateTime.Parse("2002-09-01")},
-            new users{FirstMidName="Arturo",LastName="Anand",EnrollmentDate=DateTime.Parse("2003-09-01")},
-            new users{FirstMidName="Gytis",LastName="Barzdukas",EnrollmentDate=DateTime.Parse("2002-09-01")},
-            new users{FirstMidName="Yan",LastName="Li",EnrollmentDate=DateTime.Parse("2002-09-01")},
-            new users{FirstMidName="Peggy",LastName="Justice",EnrollmentDate=DateTime.Parse("2001-09-01")},
-            new users{FirstMidName="Laura",LastName="Norman",EnrollmentDate=DateTime.Parse("2003-09-01")},
-            new users{FirstMidName="Nino",LastName="Olivetto",EnrollmentDate=DateTime.Parse("2005-09-01")}
+            new users{FirstMidName="among us",LastName="sussy impasta" },
+            new users{FirstMidName="Meredith",LastName="Alonso" },
+            new users{FirstMidName="Arturo",LastName="Anand" },
+            new users{FirstMidName="Gytis",LastName="Barzdukas"},
+            new users{FirstMidName="Yan",LastName="Li"},
+            new users{FirstMidName="Peggy",LastName="Justice"},
+            new users{FirstMidName="Laura",LastName="Norman"},
+            new users{FirstMidName="Nino",LastName="Olivetto"}
             };
             foreach (users s in users)
             {
@@ -49,26 +49,8 @@ namespace ContosoUniversity.Data
             }
             context.SaveChanges();
 
-            var enrollments = new Enrollment[]
-            {
-            new Enrollment{usersID=1,CourseID=1050,Grade=Grade.A},
-            new Enrollment{usersID=1,CourseID=4022,Grade=Grade.C},
-            new Enrollment{usersID=1,CourseID=4041,Grade=Grade.B},
-            new Enrollment{usersID=2,CourseID=1045,Grade=Grade.B},
-            new Enrollment{usersID=2,CourseID=3141,Grade=Grade.F},
-            new Enrollment{usersID=2,CourseID=2021,Grade=Grade.F},
-            new Enrollment{usersID=3,CourseID=1050},
-            new Enrollment{usersID=4,CourseID=1050},
-            new Enrollment{usersID=4,CourseID=4022,Grade=Grade.F},
-            new Enrollment{usersID=5,CourseID=4041,Grade=Grade.C},
-            new Enrollment{usersID=6,CourseID=1045},
-            new Enrollment{usersID=7,CourseID=3141,Grade=Grade.A},
-            };
-            foreach (Enrollment e in enrollments)
-            {
-                context.Enrollments.Add(e);
-            }
-            context.SaveChanges();
+            
+           
         }
     }
 }
--- ContosoUniversity/Data/SchoolContext.cs
+++ ContosoUniversity/Data/SchoolContext.cs
@@ -10,13 +10,13 @@ namespace ContosoUniversity.Data
         }
 
         public DbSet<Course> Courses { get; set; }
-        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Absence> Absences { get; set; }
         public DbSet<users> userss { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Course>().ToTable("Course");
-            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+
             modelBuilder.Entity<users>().ToTable("users");
         }
     }
