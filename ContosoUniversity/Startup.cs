10bf6d9c4577a94b534bc86689b0a9e972fecaa4
Jinne123 <70944114+Jinne123@users.noreply.github.com>
2022-06-24 13:13:59 +0200

    login + final changes

--- ContosoUniversity/Startup.cs
+++ ContosoUniversity/Startup.cs
@@ -50,6 +50,8 @@ namespace ContosoUniversity
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
