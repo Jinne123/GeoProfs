e279763c8119c769f70037ff221875f63848e5a6
markiegaming <58847729+markiegaming@users.noreply.github.com>
2022-09-05 10:55:43 +0200

    new

---
+++ ContosoUniversity/Controllers/AbsencesController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Data;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Controllers
+{
+    public class AbsencesController : Controller
+    {
+        private readonly SchoolContext _context;
+
+        public AbsencesController(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Absences
+        public async Task<IActionResult> Index()
+        {
+            
+            return View(await _context.Absences.ToListAsync());
+        }
+
+        // GET: Absences/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var absence = await _context.Absences
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (absence == null)
+            {
+                return NotFound();
+            }
+
+            return View(absence);
+        }
+
+        // GET: Absences/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Absences/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,userId,StartAbsence,StopAbsence,reason,status")] Absence absence)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(absence);
+                
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(absence);
+        }
+
+        // GET: Absences/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var absence = await _context.Absences.FindAsync(id);
+            if (absence == null)
+            {
+                return NotFound();
+            }
+            return View(absence);
+        }
+
+        // POST: Absences/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int? id, [Bind("id,userId,StartAbsence,StopAbsence,reason,status")] Absence absence)
+        {
+            if (id != absence.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(absence);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AbsenceExists(absence.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(absence);
+        }
+
+        // GET: Absences/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var absence = await _context.Absences
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (absence == null)
+            {
+                return NotFound();
+            }
+
+            return View(absence);
+        }
+
+        // POST: Absences/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            var absence = await _context.Absences.FindAsync(id);
+            _context.Absences.Remove(absence);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AbsenceExists(int? id)
+        {
+            return _context.Absences.Any(e => e.id == id);
+        }
+    }
+}
---
+++ ContosoUniversity/Models/users.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ContosoUniversity.Models
+{
+    public class users
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+    }
+}
