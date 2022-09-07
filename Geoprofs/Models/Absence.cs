62197a14560740cfcf90ba5859b391d9ca8c7317
Jinne123 <70944114+Jinne123@users.noreply.github.com>
2022-09-07 11:19:12 +0200

    name change to Geoprofs

---
+++ Geoprofs/Models/Absence.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Geoprofs.Models
+{
+    public class Absence
+    {
+        public int id { get; set; }
+        public int userId { get; set; }
+        public DateTime? StartAbsence { get; set; }
+        public DateTime? StopAbsence { get; set; }
+        public string reason { get; set; }
+        public Boolean status { get; set; }
+    }
+}
---
+++ Geoprofs/Models/users.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Geoprofs.Models
+{
+    public class users
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+    }
+}
