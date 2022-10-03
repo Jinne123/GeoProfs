bcd85b99b8086354897cc80da552399f9520d1e3
Jinne123 <70944114+Jinne123@users.noreply.github.com>
2022-10-03 10:13:07 +0200

    password confirmation

--- Geoprofs/Models/Login.cs
+++ Geoprofs/Models/Login.cs
@@ -8,13 +8,19 @@ namespace Geoprofs.Models
         public int ID { get; set; }
         [DisplayName("User name")]
         [Required(ErrorMessage ="This field is empty")]
-        public string UserName { get; set; }
+        public string? UserName { get; set; }
         [DataType(DataType.Password)]
         [Required(ErrorMessage = "This field is empty")]
-        public string Password { get; set; }
+        public string? Password { get; set; }
 
 
-        public string LoginErrorMe { get; set; }
+        public string? LoginErrorMe { get; set; }
         public int? Role { get; set; }
+        public Login(string UserName, string Password)
+        {
+            this.UserName = UserName;
+            this.Password = Password;
+            this.Role = 0;
+        }
     }
 }
